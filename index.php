5a1a26b954fd50eb93fa29237f3d8dcfa430f5d9
Nux007 <007.nux@gmail.com>
2021-01-24 15:37:43 +0100

    Exo 165.

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * 1. Créez une constante contenant le nombre 12
+ * 2. Créez une deuxième constante qui contient le nombre 2
+ * 3. Affichez le résultat de la première constante multipliée par la deuxième constante
+ * 4. Créez une constante contenant la chaîne: C'est cool PHP
+ * 5. Calculez le résultat de la première constante additionnée de la deuxième constante, le tout multiplié par la
+ *    longueur de la chaîne de la troisième constante ( attention aux priorités )
+ */
+// TODO Votre code ici.
+
+
+/**
+ * 6. Utilisez la bonne constante magique et éventuellement la bonne fonction pour afficher un maximum d'informations sur la page actuelle4
+ */
+// TODO Votre code ici.
